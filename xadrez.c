97927045b9947e6eb1c09339b2009af3eaf704a7
Javan Rosario <oliveiraajavan@hotmail.com>
2025-05-17 22:28:57 -0300

    Adicionando Voids

--- xadrez.c
+++ xadrez.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
 
+void movertorre (int casas){
+    while(casas<=5){
+        printf("Movendo torre para direita\n");
+    casas ++;
+    }
+}
+
+
+void moverrainha(int casas){
+    for(casas = 1; casas <= 8; casas++){
+        printf("Movendo rainha para esquerda\n");
+    }
+}
+// bispo com for mais complexo
+void moverbispo(int casas) {
+    do {
+        for (int i = 0; i < 2; i++) {
+            printf("Movendo para cima, direita!\n");
+        }
+        casas++;
+    } while (casas < 5);
+}
+
+
 int main(){
     int torre =0, rainha = 0, bispo =0;
     int movcompleto =1 ;    
+    printf("==================================\n"); 
+    printf("MOVIMENTO TORRE:\n");
+    movertorre(torre);
     printf("==================================\n");
-    //exemplo com while:
-    printf("MOVIMENTO TORRE:");
-    while(torre<=5){
-        printf("Movendo torre para direita\n");
-    torre ++;
-    }
+    printf("MOVIMENTO RAINHA\n");
+    moverrainha(rainha);
     printf("==================================\n");
-    //exemplo com for:
-    printf("MOVIMENTO RAINHA");
-    for(rainha = 1; rainha <= 8; rainha++){
-        printf("Movendo rainha para esquerda\n");
-    }
-    printf("==================================\n");
-    //exemplo com do-while:
-    printf("MOVIMENTO BISPO");
-    do{
-        printf("Movendo para cima, direita!\n");
-        bispo++;
-    }while (bispo < 5);
+    printf("MOVIMENTO BISPO\n");
+    moverbispo(bispo);
     printf("==================================\n");
     //exemplo com cavalo estruturas de repetição aninhadas
-    printf("MOVIMENTO CAVALO");
+    printf("MOVIMENTO CAVALO\n");
     while (movcompleto --)
     {
         for (int cavalo = 1; cavalo <= 3 ; cavalo ++)
